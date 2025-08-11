b7578a1306eaed3f5273083aab999dd44b6cbb08
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-08-11 16:09:39 -0700

    production-fix

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -56,6 +56,11 @@ export const entrySchema = z
     }
   );
 
+export const coverLetterSchema = z.object({
+  companyName: z.string().min(1, "Company name is required"),
+  jobTitle: z.string().min(1, "Job title is required"),
+  jobDescription: z.string().min(1, "Job description is required"),
+});
 
 export const resumeSchema = z.object({
   contactInfo: contactSchema,
