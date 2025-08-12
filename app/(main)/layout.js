5fb70bd1ca4c74c296f48323d9c74000186464ff
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-08-11 18:49:57 -0700

    added-log

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -3,6 +3,9 @@ import Header from "@/components/header";
 
 const RootLayoutPage = ({children}) => {
     // Redirect user after Onboarding
+    if (process.env.VERCEL) {
+  console.log("Has Clerk PK at build?", Boolean(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY));
+}
   return (
     <div className='container mx-auto mt-24 mb-20'>
         <Header />
